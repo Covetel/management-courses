8af107b6329bd8a6628ee9c6b350e46bab490781
ajpadilla <ajpadilla88@gmail.com>
2014-01-14 14:54:05 -0430

    generando vistas management course

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+        "name": "Management - Course",
+        "version": "1.0",
+        "depends": ["base"],
+        "author": "Author Name",
+        "category": "Test",
+        "description": "",
+        "update_xml": ["management_course_view.xml",],
+        "installable": True,
+        "active": True,
+}

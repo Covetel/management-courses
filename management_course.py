7f8fd06cb534c080fe6c95ef0de3344d87d4d222
ajpadilla <ajpadilla88@gmail.com>
2014-01-14 15:08:30 -0430

    agregando lista de instructores

--- management_course.py
+++ management_course.py
@@ -11,7 +11,7 @@ class course(osv.osv):
               "start_date" : fields.date("Stard Date"),
               "end_date":fields.date("End Date"),
               "hours" : fields.float("Hours",digits=(6,2),help="Duration"),
-              #"instructor": fields.one2many("res.users","user_id","Instructor"),
+              "instructor_ids": fields.one2many("res.users","user_id","Instructor"),
               "attendee_ids": fields.one2many("res.partner","partner_id","Attendee"),
               "place": fields.char("Place of course",size=40,required=True),
             }
@@ -21,8 +21,19 @@ class partner(osv.osv):
  _name = "res.partner"
  _description = "Attendee"
  _inherit = "res.partner"
+
  _columns = {
               "partner_id" : fields.many2one("management.course","Course",required=True,ondelete="cascade"),
             } 
 partner()
 
+class user(osv.osv):
+ _name = "res.users"
+ _inherit = "res.users"
+ _description = "Instructor" 
+
+ _columns = {
+              "user_id" : fields.many2one("management.course","Course",required=True,ondelete="cascade"),
+            }
+
+user()

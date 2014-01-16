733648b5e73a624ed02c96a74b6bb79105fe11f4
ajpadilla <ajpadilla88@gmail.com>
2014-01-16 14:58:52 -0430

    Agregando vista instructor

--- management_course.py
+++ management_course.py
@@ -7,12 +7,13 @@ class course(osv.osv):
  _description = "Course"
  
  _columns = {
-              "course_name" : fields.char("Course Name",size=30,required=True),
+              "course_name" : fields.char("Course Name",size=256,required=True),
               "start_date" : fields.date("Start Date"),
               "end_date":fields.date("End Date"),
               "hours" : fields.float("Hours",digits=(6,2),help="Duration"),
+              "instructor_ids": fields.one2many("instructor","partner_id","Instructor"),
               "attendee_ids": fields.one2many("res.partner","partner_id","Attendee"),
-              "place": fields.char("Place of course",size=40,required=True),
+              "place": fields.char("Place of course",size=256,required=True),
               "technical_requirement" : fields.one2many("technical.requirement.course","requirement_course","Technical Requirement"),
             }
 course()
@@ -25,16 +26,23 @@ class partner(osv.osv):
  _columns = {
               "partner_id" : fields.many2one("management.course","Course",required=True,ondelete="cascade"),
               "cedula_rif" : fields.char("Cedula-Rif  ",size=12,required=True),
-              "is_instructor" : fields.boolean("Instructor"),
             } 
 partner()
+
+class partner_instructor(osv.osv):
+ _name = "instructor"
+ _description = "Course Instructor"
+ _inherit = "res.partner"
+ _table = ""
+partner_instructor()
 
 class technical_requirement_course(osv.osv):
  _name = "technical.requirement.course"
  _description = "Technical Requirement Course"
  
  _columns = {
-              "requirement" : fields.char("Requirement",size=40,required=True),
+              "requirement" : fields.char("Requirement",size=256,required=True),
               "requirement_course": fields.many2one("management.course","Requirement Course",required=True,ondelete="cascade"),
             }
 technical_requirement_course()
+

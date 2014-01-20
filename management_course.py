1d0944046b91e4e39b881f7330e91831f5634fc7
Luis Alfredo da Silva <luisdasilva@covetel.com.ve>
2014-01-20 01:29:09 -0430

    changind name attendee to participants, to match better

--- management_course.py
+++ management_course.py
@@ -17,7 +17,7 @@ class course(osv.osv):
               "start_date" : fields.date("Start Date"),
               "end_date":fields.date("End Date"),
               "hours" : fields.float("Hours",digits=(6,2),help="Duration"),
-              "attendee_ids": fields.one2many("attendee","course_id","Attendee"),
+              "participant_ids": fields.one2many("participant","course_id","Participant"),
               "place": fields.char("Place of course",size=256,required=True),
               "technical_requirement" : fields.one2many("technical.requirement.course","requirement_course","Technical Requirement"),
               "certificate_pdf" : fields.binary("File", readonly=True),
@@ -26,8 +26,8 @@ class course(osv.osv):
 course()
 
 
-class partner_attendee(osv.osv):
- _name = "attendee"
+class partner_participant(osv.osv):
+ _name = "participant"
  _description = "Course Instructor"
  
  _columns = {
@@ -35,7 +35,7 @@ class partner_attendee(osv.osv):
             "cedula_rif" : fields.char("Cedula-Rif",size=12),
             "course_id" : fields.many2one("management.course","Course",required=True,ondelete="cascade"),
             }
-partner_attendee()
+partner_participant()
 
 
 class technical_requirement_course(osv.osv):

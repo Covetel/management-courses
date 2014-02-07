46ee97a30ecdc67c0df5c864c7a349c01cce4afe
Carlos Paredes <cparedes@covetel.com.ve>
2014-02-07 13:43:39 -0430

    Corriendo nombre de la categoria
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- __openerp__.py
+++ __openerp__.py
@@ -3,10 +3,12 @@
         "version": "1.0",
         "depends": ["base"],
         "author": "Covetel R.S",
-        "category": "Management Courses",
+        "category": "Management Course",
         "description": "OpenERP module allowing you to manage your courses, technical requirements, participants, instructors, etc.",
         "data": [
-            "management_course_view.xml"
+            "security/management_course_rules.xml",
+            #"security/ir.model.access.csv",
+            "management_course_view.xml",
         ],
         "installable": True,
         "active": False,

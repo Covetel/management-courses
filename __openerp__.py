52973e11eb4c318bcd8515f70b9f199a067f029b
Carlos Paredes <cparedes@covetel.com.ve>
2014-02-07 11:58:08 -0430

    Corrigiendo información y categoria del módulo
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- __openerp__.py
+++ __openerp__.py
@@ -3,8 +3,8 @@
         "version": "1.0",
         "depends": ["base"],
         "author": "Covetel R.S",
-        "category": "Test",
-        "description": "",
+        "category": "Management Courses",
+        "description": "OpenERP module allowing you to manage your courses, technical requirements, participants, instructors, etc.",
         "data": [
             "management_course_view.xml"
         ],

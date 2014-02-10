a8c30642f6386e2e2524173d0bdcce1382ab04fd
Luis Alfredo da Silva <luisdasilva@covetel.com.ve>
2014-02-10 18:38:07 -0430

    generated certificates with rml

--- report/report_certificates.py
+++ report/report_certificates.py
@@ -4,10 +4,11 @@ from openerp.report import report_sxw
 class certificates(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context=None):
         super(certificates, self).__init__(cr, uid, name, context=context)
+
         self.localcontext.update({
             'time': time,
         })
 
 report_sxw.report_sxw('report.management.course', 'management.course', 
-                      '/vagrant/OpenERP-Modules/management-courses/report/report_certificates.rml', 
+                      '/home/xyklex/OpenERP-Modules/management-courses/report/report_certificates.rml', 
                       parser=certificates)

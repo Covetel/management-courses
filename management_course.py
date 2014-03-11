d5256bdb74d8fe65f12bf052d6a9faed09b843c4
ajpadilla <ajpadilla88@gmail.com>
2014-03-11 16:54:05 -0430

    Actualizando el atributo _description del modelo Course

--- management_course.py
+++ management_course.py
@@ -12,8 +12,7 @@ _logger = logging.getLogger(__name__)
 
 class course(osv.osv):
     _name = "management.course"
-    _description = "managemenet.course"
-    _table = "management_course"
+    _description = "Managemenet Course"
     
     def get_certificates(self, cr, uid, ids, context=None):
         this = self.browse(cr, uid, ids)[0]

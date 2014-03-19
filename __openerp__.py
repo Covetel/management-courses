1ce05268d57d8aad4b31e326bfc1ba692eab7769
Carlos Paredes <cparedes@covetel.com.ve>
2014-03-19 12:11:10 -0430

    Añadiendo creación de grupos de seguridad para el módulo
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- __openerp__.py
+++ __openerp__.py
@@ -7,6 +7,7 @@
         "description": "OpenERP module allowing you to manage your courses, technical requirements, participants, instructors, etc.",
         "data": [
             "security/management_course_rules.xml",
+            #"security/ir.model.access.csv",
             "management_course_view.xml",
         ],
         "installable": True,
